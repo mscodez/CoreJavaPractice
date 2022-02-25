a07f72e2d4a7c47cdb8cc0ca6166a8983fa4d9f2
shadab <ms.codezone@gmail.com>
2022-02-25 12:04:07 +0530

    by me

---
+++ Lab231.java
@@ -0,0 +1,12 @@
+class Lab231{
+	int a;
+	public static void main(String args[]){
+		System.out.println(a);
+	}
+}
+
+/*Lab231.java:4: error: non-static variable a cannot be 
+referenced from a static context
+                System.out.println(a);
+                                   ^
+1 error*/
